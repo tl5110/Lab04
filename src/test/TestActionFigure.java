1ab1d8988e8e52fbdec2531bc8a625c26d21ea24
Tiffany Lee <tl5110@g.rit.edu>
2023-02-09 00:46:45 -0500

    Updated file with the fixed starter code

--- src/test/TestActionFigure.java
+++ src/test/TestActionFigure.java
@@ -30,7 +30,7 @@ public class TestActionFigure {
     @Order(1)
     public void testFirstActionFigure() {
         ActionFigure af1 = (ActionFigure) ToyFactory.makeToy("ACTION_FIGURE He-man 30 By_the_power_of_Grayskull!");
-        assertEquals(200, af1.getProductCode());
+        assertEquals(500, af1.getProductCode());
         assertEquals("He-man", af1.getName());
         assertEquals(0, af1.getHappiness());
         assertFalse(af1.isRetired());
@@ -39,10 +39,10 @@ public class TestActionFigure {
         assertEquals(30, af1.getAge());
         assertEquals(1, af1.getEnergyLevel());
         assertEquals("By_the_power_of_Grayskull!", af1.getSpeak());
-        assertEquals("Toy{PC:200, N:He-man, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:1}", af1.toString());
+        assertEquals("Toy{PC:500, N:He-man, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:1}", af1.toString());
 
         af1.play(40);
-        String expected = "PLAYING(40): Toy{PC:200, N:He-man, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:1}" + System.lineSeparator();
+        String expected = "PLAYING(40): Toy{PC:500, N:He-man, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:1}" + System.lineSeparator();
         expected += "\tHe-man kung foo chops with 40 energy!" + System.lineSeparator();
         expected += "\tHe-man brushes their ORANGE hair and says, \"By_the_power_of_Grayskull!\"";
         assertEquals(expected, outputStreamCaptor.toString()
@@ -55,7 +55,7 @@ public class TestActionFigure {
 
         outputStreamCaptor.reset();
         af1.play(12);
-        expected = "PLAYING(12): Toy{PC:200, N:He-man, H:40, R:false, W:30.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:2}" + System.lineSeparator();
+        expected = "PLAYING(12): Toy{PC:500, N:He-man, H:40, R:false, W:30.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:2}" + System.lineSeparator();
         expected += "\tHe-man kung foo chops with 24 energy!" + System.lineSeparator();
         expected += "\tHe-man brushes their ORANGE hair and says, \"By_the_power_of_Grayskull!\"";
         assertEquals(expected, outputStreamCaptor.toString()
@@ -68,10 +68,10 @@ public class TestActionFigure {
 
         outputStreamCaptor.reset();
         af1.play(49);
-        expected = "PLAYING(49): Toy{PC:200, N:He-man, H:52, R:false, W:60.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:3}" + System.lineSeparator();
+        expected = "PLAYING(49): Toy{PC:500, N:He-man, H:52, R:false, W:60.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:3}" + System.lineSeparator();
         expected += "\tHe-man kung foo chops with 147 energy!" + System.lineSeparator();
         expected += "\tHe-man brushes their ORANGE hair and says, \"By_the_power_of_Grayskull!\"" + System.lineSeparator();
-        expected += "RETIRED: Toy{PC:200, N:He-man, H:101, R:true, W:90.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:4}";
+        expected += "RETIRED: Toy{PC:500, N:He-man, H:101, R:true, W:90.0}, Doll{HC:ORANGE, A:30, S:By_the_power_of_Grayskull!}, ActionFigure{E:4}";
         assertEquals(expected, outputStreamCaptor.toString()
                 .trim());
         assertEquals(101, af1.getHappiness());
@@ -85,7 +85,7 @@ public class TestActionFigure {
     @Order(2)
     public void testSecondActionFigure() {
         ActionFigure af2 = (ActionFigure) ToyFactory.makeToy("ACTION_FIGURE Animal 27 BEAT_DRUMS!");
-        assertEquals(201, af2.getProductCode());
+        assertEquals(501, af2.getProductCode());
         assertEquals("Animal", af2.getName());
         assertEquals(0, af2.getHappiness());
         assertFalse(af2.isRetired());
@@ -94,10 +94,10 @@ public class TestActionFigure {
         assertEquals(27, af2.getAge());
         assertEquals(1, af2.getEnergyLevel());
         assertEquals("BEAT_DRUMS!", af2.getSpeak());
-        assertEquals("Toy{PC:201, N:Animal, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:27, S:BEAT_DRUMS!}, ActionFigure{E:1}", af2.toString());
+        assertEquals("Toy{PC:501, N:Animal, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:27, S:BEAT_DRUMS!}, ActionFigure{E:1}", af2.toString());
 
         af2.play(40);
-        String expected = "PLAYING(40): Toy{PC:201, N:Animal, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:27, S:BEAT_DRUMS!}, ActionFigure{E:1}" + System.lineSeparator();
+        String expected = "PLAYING(40): Toy{PC:501, N:Animal, H:0, R:false, W:0.0}, Doll{HC:ORANGE, A:27, S:BEAT_DRUMS!}, ActionFigure{E:1}" + System.lineSeparator();
         expected += "\tAnimal kung foo chops with 40 energy!" + System.lineSeparator();
         expected += "\tAnimal brushes their ORANGE hair and says, \"BEAT_DRUMS!\"";
         assertEquals(expected, outputStreamCaptor.toString()
